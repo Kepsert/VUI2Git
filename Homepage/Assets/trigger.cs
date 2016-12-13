10c87f8a311e436aefccf21a081ed3c100b0617a
Tim Reuten <t.reuten@student.fontys.nl>
2016-12-13 15:07:08 +0100

    MEGAPUSH

---
+++ Homepage/Assets/trigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class trigger : MonoBehaviour {
+
+	public Text txt;
+
+	private bool productCounted = false;
+	private int productCounter = 0;
+
+	// Use this for initialization
+	void Start () {
+		txt.text = "Products in cart:" + productCounter;
+	}
+
+	void Update() {
+		if (productCounted) {
+			txt.text = "Products in cart:" + productCounter;
+			productCounted = false;
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Product") {
+			productCounter++;
+			productCounted = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.tag == "Product") {
+			productCounter--;
+			productCounted = true;
+		}
+	}
+}
